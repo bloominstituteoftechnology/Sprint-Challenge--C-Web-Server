17eeef173214ebd1f886e956b03d61ce02123e79
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-26 14:44:20 -0500

    fix parse_url and connect server

--- src/client.c
+++ src/client.c
@@ -41,36 +41,26 @@ urlinfo_t *parse_url(char *url)
   // We can parse the input URL by doing the following:
 
   // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-  first_backslash = strchr(url, '/');
+  char *temp = strchr(hostname, '/');
 
   // 2. Set the path pointer to 1 character after the spot returned by strchr.
-  path = &first_backslash[1];
+  path = temp + 1;
 
   // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-  for (int i = 0; i < strlen(hostname); i++)
-  {
-    if (hostname[i] == '/')
-    {
-      hostname[i] = '\0';
-      return 0;
-    }
-  }
+  *temp = '\0';
 
   // 4. Use strchr to find the first colon in the URL.
-  first_colon = strchr(url, ':');
+  temp = strchr(hostname, ':');
 
   // 5. Set the port pointer to 1 character after the spot returned by strchr.
-  port = &first_colon[1];
+  port = temp + 1;
 
   // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  for (int i = 0; i < strlen(hostname); i++)
-  {
-    if (hostname[i] == ':')
-    {
-      hostname[i] = '\0';
-      return 0;
-    }
-  }
+  *temp = '\0';
+
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
@@ -130,7 +120,14 @@ int main(int argc, char *argv[])
   send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
 
   // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+  while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0)
+  {
+    fprintf(stdout, "%s\n", buf);
+  }
+
   // 5. Clean up any allocated memory and open file descriptors.
+  free(urlinfo);
+  close(sockfd);
 
   return 0;
 }
