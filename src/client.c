ccb54e0691c1b7648c409138d676713d1ac17d73
JJ Ashcraft <jj@jjashcraft.com>
2018-10-26 14:41:28 -0400

    problem with linker in compiler...moved get_socket function to client.c for troubleshooting. now works. mvp complete

--- src/client.c
+++ src/client.c
@@ -7,7 +7,70 @@
 #include <sys/socket.h>
 #include "lib.h"
 
+
+
 #define BUFSIZE 4096 // max number of bytes we can get at once
+
+int get_socket(char *hostname, char *port)
+{
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char s[INET6_ADDRSTRLEN];
+
+  // This block of code looks at the local network interfaces and
+  // tries to find some that match our requirements (namely either
+  // IPv4 or IPv6 (AF_UNSPEC) and TCP (SOCK_STREAM) and use any IP on
+  // this machine (AI_PASSIVE).
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0)
+  {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    exit(1);
+  }
+
+  // Once we have a list of potential interfaces, loop through them
+  // and try to set up a socket on each. Quit looping the first time
+  // we have success.
+  for (p = servinfo; p != NULL; p = p->ai_next)
+  {
+    // Try to make a socket based on this candidate interface
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      perror("client: socket");
+      continue;
+    }
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sockfd);
+      perror("client: connect");
+      continue;
+    }
+
+    // If we got here, we got a bound socket and we're done
+    break;
+  }
+
+  // If p is NULL, it means we didn't break out of the above loop
+  // and we don't have a good socket.
+  if (p == NULL)
+  {
+    fprintf(stderr, "client: failed to connect\n");
+    exit(2);
+  }
+
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof(s));
+  printf("client: connecting to %s\n", s);
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  return sockfd;
+}
 
 /**
  * Struct to hold all three pieces of a URL
