982f5eda5562e970ecea9c95ea5465c8e1f9cff8
Simon <Simon_Freelance@hotmail.com>
2018-10-26 12:09:35 -0400

    parse_url completed

--- src/client.c
+++ src/client.c
@@ -31,27 +31,27 @@ urlinfo_t *parse_url(char *url)
   char *hostname = strdup(url);
   char *port;
   char *path;
+  
+  char *end = strchr(hostname, ':');
 
+  port = end + 1;
+
+  *end = '\0';
+  end = strchr(port, '/');
+
+  path = end + 1;
+
+  *end = '\0';
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
+  
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
-  /*
-    We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
-
+  printf("Hostname: %s\n Port: %s\n Path: %s\n", hostname, port, path);
+  
   return urlinfo;
 }
-
 /**
  * Constructs and sends an HTTP request
  *
