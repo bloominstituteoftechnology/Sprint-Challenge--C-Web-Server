2978562fb60429a1a3299c8cb42d78028be32a9b
JacobLayton <jlayjlay3@gmail.com>
2018-10-26 10:50:01 -0700

    Bug when trying to connect with server, not parsing correctly

--- src/client.c
+++ src/client.c
@@ -122,9 +122,15 @@ int main(int argc, char *argv[])
     5. Clean up any allocated memory and open file descriptors.
   */
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
-
+  urlinfo_t *urlinfo = parse_url(argv[1]);
+  sockfd = get_socket(urlinfo->hostname, urlinfo->port);
+  send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+  while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0) {
+  // print the data we got back to stdout
+    printf("%s\n", buf);
+  }
+  free(urlinfo);
+  close(sockfd);
+  
   return 0;
 }
