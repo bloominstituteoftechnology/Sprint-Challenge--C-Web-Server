6ebdf18644ac989dea7fd70695c8c608026b0596
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-10-26 17:44:56 -0400

    server runs

--- src/client.c
+++ src/client.c
@@ -93,6 +93,20 @@ int send_request(int fd, char *hostname, char *port, char *path)
   // IMPLEMENT ME! //
   ///////////////////
 
+  int request_length = sprintf(request,
+    "GET /%s HTTP/1.1\n"
+    "Host: %s:%s\n"
+    "Connection: close\n"
+    "\n",
+    path,
+    hostname,
+    port
+  );
+
+  if ((rv = send(fd, request, request_length, 0) < 0)) {
+    perror("error sending request");
+  }
+
   return 0;
 }
 
