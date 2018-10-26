3424f4a8dddccc1d52b68966fd4d470f82c21632
Erin Meredith <ekmeredith@att.net>
2018-10-26 16:18:28 -0400

    fixed request error

--- src/client.c
+++ src/client.c
@@ -74,7 +74,8 @@ int send_request(int fd, char *hostname, char *port, char *path)
   int request_length = sprintf(request,
     "GET /%s HTTP/1.1\n"
     "Host: %s:%s\n"
-    "Connection: close\n", 
+    "Connection: close\n"
+    "\n", 
     path, hostname, port
     );
 
