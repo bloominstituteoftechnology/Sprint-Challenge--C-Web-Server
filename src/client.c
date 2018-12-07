71ef189944abfc8a2a4baca673ae7c99c01b01be
katiagilligan888 <katia.gilligan88@gmail.com>
2018-12-07 10:30:03 -0700

    Added the send_request funtion

--- src/client.c
+++ src/client.c
@@ -78,9 +78,11 @@ int send_request(int fd, char *hostname, char *port, char *path)
   char request[max_request_size];
   int rv;
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  int request_length = sprintf(request, "GET /%s HTTP/1.1\n Host: %s:%s\n Connection: close\n\n", path, hostname, port); 
+  
+  printf("%s", request); 
+
+  rv = send(fd, request, request_length, 0);
 
   return 0;
 }
