25ef0c52ce94b9b930c03b68de2f524034308219
AI2EJAY <jhammett1314@gmail.com>
2018-12-07 10:36:44 -0600

    Parse URL rough done working on http request

--- src/client.c
+++ src/client.c
@@ -34,16 +34,22 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+  
+    // We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    //1.Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+    char *bs = strchr(url, "/");
+    printf("|||||||||||||||||| test\n");
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
+    path = bs + 1; //doesn't seem right going to have to look at that
+    // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+    bs = "\0";
+    // 4. Use strchr to find the first colon in the URL.
+    char *col = strchr(url, ":");
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+    port = col + 1;
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+    col = "\0";
 
   ///////////////////
   // IMPLEMENT ME! //
