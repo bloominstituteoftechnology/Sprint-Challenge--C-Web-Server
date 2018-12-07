110665838b8bb2f6f120b9456b7738e395c4caea
DasGMA <dasgrigoma@gmail.com>
2018-12-07 08:24:45 -0800

    Send request

--- src/client.c
+++ src/client.c
@@ -34,20 +34,26 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
-
+  /* 
     1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
     2. Set the path pointer to 1 character after the spot returned by strchr.
     3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+  */
+  path = strchr(hostname, '/') + 1;
+  *path = '\0';
+
+/*
     4. Use strchr to find the first colon in the URL.
     5. Set the port pointer to 1 character after the spot returned by strchr.
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+*/
+  port = strchr(hostname, ':') + 1;
+  *port = '\0';
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+// Next three lines of code storing strings in urlinfo structure
+  urlinfo -> hostname = hostname;
+  urlinfo -> port = port;
+  urlinfo -> path = path;
 
   return urlinfo;
 }
@@ -72,7 +78,21 @@ int send_request(int fd, char *hostname, char *port, char *path)
   // IMPLEMENT ME! //
   ///////////////////
 
-  return 0;
+  int request_length = sprintf(
+        request,
+        "GET %s HTTP/1.1\nHOST: %s%s\nConnection: Close\n\n",
+        path, hostname, port
+    );
+
+// Send it all........
+  rv = send(fd, request, request_length, 0);
+
+  if (rv < 0) 
+  {
+    perror("Send");
+  }
+
+  return rv;
 }
 
 int main(int argc, char *argv[])
