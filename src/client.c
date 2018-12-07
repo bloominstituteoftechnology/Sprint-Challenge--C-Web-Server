e47f3f7829efc0cb4870af9f18012c89ec98e7a6
vanceleon <vanceleon44@gmail.com>
2018-12-07 11:56:00 -0500

    parse and send request are implemented might have bugs in the functions working on main function

--- src/client.c
+++ src/client.c
@@ -34,21 +34,29 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+  
+    // We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+    char backslash = strchr(url, "/");
+    while(backslash){
+      urlinfo->path = 1;
+      backslash = "\0";
+    };
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
 
+    // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+    
+    // 4. Use strchr to find the first colon in the URL.
+    char colon = strchr(url, ":");
+    while (colon) {
+      urlinfo->port = 1;
+      colon = "\0";
+    }
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+    
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   return urlinfo;
 }
 
@@ -67,10 +75,15 @@ int send_request(int fd, char *hostname, char *port, char *path)
   const int max_request_size = 16384;
   char request[max_request_size];
   int rv;
-
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  int request_length = sprintf(request, "File Descriptor: %d\nHostname: %s\nPort: %s\nPath:%s\n", fd, hostname, port, path);
+
+  rv = send(fd, request, request_length, 0);
+  if(rv < 0){
+    perror("send");
+  }
 
   return 0;
 }
@@ -87,6 +100,7 @@ int main(int argc, char *argv[])
 
   /*
     1. Parse the input URL
+    
     2. Initialize a socket
     3. Call send_request to construct the request and send it
     4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
@@ -96,6 +110,9 @@ int main(int argc, char *argv[])
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+
+  parse_url(argv);
+
 
   return 0;
 }
