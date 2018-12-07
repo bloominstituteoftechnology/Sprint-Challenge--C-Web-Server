fb655f26ba6b2589c939d341b79e7ddffde87474
mkerbleski <mkerbles@gmail.com>
2018-12-07 09:50:29 -0700

    implement parse_url function

--- src/client.c
+++ src/client.c
@@ -34,20 +34,44 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+  
+  // We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+  // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+  printf("in parse_url\n");
+  path = strchr(url, '/');
+  printf("path: %s\n", path);
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // char *hostname = url;
+  // printf("port: %s", port);
+
+  // 2. Set the path pointer to 1 character after the spot returned by strchr.
+
+  urlinfo->path = path +1;
+  printf("urlinfo->path: %s\n", urlinfo->path);
+
+  // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+
+  *path = '\0';
+  printf("url: %s\n", url);
+
+  // 4. Use strchr to find the first colon in the URL.
+
+  port = strchr(url, ':');
+  printf("port: %s\n", port);
+
+  // 5. Set the port pointer to 1 character after the spot returned by strchr.
+  
+  urlinfo->port = port +1;
+  printf("urlinfo->port: %s\n", urlinfo->port);
+
+  // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+
+  *port = '\0';
+  printf("url: %s\n", url);
+
+  urlinfo->hostname = url;
+  printf("urlinfo->hostname: %s\n", urlinfo->hostname);
 
   return urlinfo;
 }
@@ -87,11 +111,11 @@ int main(int argc, char *argv[])
 
   //1. Parse the input URL
   char *hostname = strdup(argv[1]);
-  printf("%s", hostname);
+  printf("%s\n", hostname);
   struct urlinfo_t *parsedUrl = parse_url(hostname);
-  printf("hostname: %s", parsedUrl->hostname);
-  printf("path: %s", parsedUrl->path);
-  printf("port: %s", parsedUrl->port);
+  printf("hostname: %s\n", parsedUrl->hostname);
+  printf("path: %s\n", parsedUrl->path);
+  printf("port: %s\n", parsedUrl->port);
   //2. Initialize a socket
   //3. Call send_request to construct the request and send it
   //4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
