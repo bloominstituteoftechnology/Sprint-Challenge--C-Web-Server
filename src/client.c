e5e3cd972b1c77591af47926fbf5c8a3004d51a4
francistse23 <wtse.ucsd@gmail.com>
2018-12-07 08:16:02 -0800

    Francis Tse - Sprint Challenge - Web Server

--- src/client.c
+++ src/client.c
@@ -49,6 +49,14 @@ urlinfo_t *parse_url(char *url)
   // IMPLEMENT ME! //
   ///////////////////
 
+  char *backslash = strchr(hostname, "/");
+  path = backslash + 1;
+  *backslash = "\0";
+
+  char *colon = strchr(hostname, ":");
+  port = colon + 1;
+  *colon = "\0";
+
   return urlinfo;
 }
 
