a15fb44cabe262e8e163cd7f77fa0caf6bdb5f49
tulf <timzulf@gmail.com>
2018-12-07 13:47:24 -0500

    cleaned up send_request to remove char artifacts.

--- src/client.c
+++ src/client.c
@@ -80,9 +80,7 @@ int send_request(int fd, char *hostname, char *port, char *path)
   int rv;
 
   // Build HTTP response and store it in response
-    int request_length = 0;
-
-    request_length = sprintf(request, "GET /%s HTTP/1.1\nHost: %s:%s\n Connection: close \n", path, hostname, port);
+    int request_length = sprintf(request, "GET /%s HTTP/1.1\nHost: %s:%s\nConnection: close\n\n", path, hostname, port);
 
     // Send it all!
     rv = send(fd, request, request_length, 0);
@@ -92,8 +90,6 @@ int send_request(int fd, char *hostname, char *port, char *path)
     }
 
     return rv;
-
-  return 0;
 }
 
 int main(int argc, char *argv[])
@@ -110,23 +106,21 @@ int main(int argc, char *argv[])
   struct urlinfo_t *urlinfo = parse_url(argv[1]);
   // 2. Initialize a socket
   sockfd = get_socket(urlinfo->hostname, urlinfo->port);
-  // 3. Call send_request to construct the request and send it
+  // 3. Call send_request to construct the request and send it send the request string down the socket.
   send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
   // 4. Call `recv` in a loop *until there is no more data* to receive from the server. Print the received response to stdout.
   //Upon successful completion, recv() shall return the length of the message in bytes. If no messages are available to be received and the peer has performed an orderly shutdown, recv() shall return 0. Otherwise, -1 shall be returned and errno set to indicate the error.
+
   while((numbytes = recv(sockfd, buf, BUFSIZE -1, 0)) > 0){
   // print the data we got back to stdout
     printf("%s\n", buf);
   }
+
   // 5. Clean up any allocated memory and open file descriptors.
   //Don't forget to `free` any allocated memory
   free(urlinfo);
   //`close` any open file descriptors.
   close(sockfd);
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
 
   return 0;
 }
