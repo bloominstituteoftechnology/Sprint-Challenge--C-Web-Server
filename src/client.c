94e169536e55b71304355a9d6e781f533d5fc2ca
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-07 09:54:47 -0800

    Storing hostname, port and path in urlinfo_t struct.

--- src/client.c
+++ src/client.c
@@ -37,21 +37,26 @@ urlinfo_t *parse_url(char *url)
     // We can parse the input URL by doing the following:
 
     // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    path = strchr(urlinfo, '/');
+    char *a = strchr(url, '/');
     // 2. Set the path pointer to 1 character after the spot returned by strchr.
-    path+=1; 
+    path = a + 1; 
     // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    *path = '\0';
+    a = '\0';
     // 4. Use strchr to find the first colon in the URL.
-    port = strchr(urlinfo, ':');
+    char *b = strchr(url, ':');
     // 5. Set the port pointer to 1 character after the spot returned by strchr.
-    port+=1;
+    port = b + 1;
     // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-    *port = '\0';
+    b = '\0';
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+    urlinfo->hostname = malloc(strlen(hostname));
+    strcpy(urlinfo->hostname, hostname);
+
+    urlinfo->port = malloc(strlen(port));
+    strcpy(urlinfo->port, port);
+
+    urlinfo->path = malloc(strlen(path));
+    strcpy(urlinfo->path, path);
 
   return urlinfo;
 }
