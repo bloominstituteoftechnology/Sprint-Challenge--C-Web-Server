df1aa4ba1c3d20546423010c1e0da64408cce349
AI2EJAY <jhammett1314@gmail.com>
2018-12-07 12:53:39 -0600

    researching printing to stdout

--- src/client.c
+++ src/client.c
@@ -42,16 +42,17 @@ urlinfo_t *parse_url(char *url)
     printf("backslash =======> %c\n", bs);
     
     // 2. Set the path pointer to 1 character after the spot returned by strchr.
-    path = bs + 1; //doesn't seem right going to have to look at that
+    urlinfo->path = bs + 1; //doesn't seem right going to have to look at that
     // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
     *bs = '\0';
     printf("backslash =======> %c\n", bs);
     // 4. Use strchr to find the first colon in the URL.
     char *col = strchr(hostname, ':');
     // 5. Set the port pointer to 1 character after the spot returned by strchr.
-    port = col + 1;
+    urlinfo->port = col + 1;
     // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
     *col = '\0';
+    urlinfo->hostname = hostname;
 
     printf("colon =======> %c\n", col);
     printf("hostname =======> %s\n", hostname);
@@ -115,14 +116,17 @@ int main(int argc, char *argv[])
     printf("argv =========> %s\n", argv[1]);
     // strchr
     // 1. Parse the input URL
-    parse_url(argv[1]);
+    urlinfo_t *urlinfo = parse_url(argv[1]);
     // 2. Initialize a socket
 
-    get_socket(urlinfo->hostname, urlinfo->path);
+    sockfd = get_socket(urlinfo->hostname, urlinfo->port);
     // 3. Call send_request to construct the request and send it
-    send_request(fd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+    numbytes = send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
     // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
-    
+    while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0) {
+       // print the data we got back to stdout
+      //  printf("%s\n", )
+     }
     // 5. Clean up any allocated memory and open file descriptors.
 
 
