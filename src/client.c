e745795bd7e9d7b63e32fbb9a2234ded45836e01
JoCatherine Griggs <jocafrin@gmail.com>
2018-12-07 12:43:51 -0500

    initial commit

--- src/client.c
+++ src/client.c
@@ -34,16 +34,25 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    // We can parse the input URL by doing the following:
+
+    // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+    path = strchr(hostname, "/");
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
+    *path = '\0';
+    path++;
+    // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+      char *overwrite = strchr(hostname, '/');
+  *overwrite = '\0';
+    // 4. Use strchr to find the first colon in the URL.
+    port = strchr(hostname, ':');
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+    port += 1;
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  char *dbl_overwrite = strchr(hostname,':');
+  *dbl_overwrite = '\0';
+  hostname = strdup(url);
 
   ///////////////////
   // IMPLEMENT ME! //
@@ -76,8 +85,8 @@ int send_request(int fd, char *hostname, char *port, char *path)
 }
 
 int main(int argc, char *argv[])
-{  
-  int sockfd, numbytes;  
+{
+  int sockfd, numbytes;
   char buf[BUFSIZE];
 
   if (argc != 2) {
