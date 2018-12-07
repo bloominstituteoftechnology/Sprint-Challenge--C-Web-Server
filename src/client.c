cec1fafce86891689f404653319d1246d964308b
root <root@DESKTOP-AHIGT3D.localdomain>
2018-12-07 11:36:01 -0500

    parse_url() implemented

--- src/client.c
+++ src/client.c
@@ -36,7 +36,7 @@ urlinfo_t *parse_url(char *url)
 
   /*
     We can parse the input URL by doing the following:
-
+    
     1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
     2. Set the path pointer to 1 character after the spot returned by strchr.
     3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
@@ -44,6 +44,21 @@ urlinfo_t *parse_url(char *url)
     5. Set the port pointer to 1 character after the spot returned by strchr.
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
+
+  char *backslash = strchr(url, '/');
+  backslash++;
+  path = backslash;
+  backslash--;
+  *backslash = '\0';
+
+  char *colon = strchr(url, ':');
+  colon++;
+  port = colon; 
+  port--; 
+  *port = '\0';
+
+  hostname = url; 
+  printf("this is hostname %s\n\n", hostname); 
 
   ///////////////////
   // IMPLEMENT ME! //
@@ -93,6 +108,7 @@ int main(int argc, char *argv[])
     5. Clean up any allocated memory and open file descriptors.
   */
 
+    parse_url(argv[1]);
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
