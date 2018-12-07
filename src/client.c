78ed58ec505e554e1884aaf1d2cc6c544d9f4710
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-12-07 12:36:39 -0600

    Parse URL and send request

--- src/client.c
+++ src/client.c
@@ -34,20 +34,30 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+  
+    //We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    //1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+   // 2. Set the path pointer to 1 character after the spot returned by strchr.
+    //3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+   // 4. Use strchr to find the first colon in the URL.
+    //5. Set the port pointer to 1 character after the spot returned by strchr.
+   // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  
 
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  char *backslash = strchr(hostname, '/');
+  path = backslash++;
+  *backslash = '\0';
+  char *colon = strchr(hostname, ':');
+  port = colon++;
+   *colon = '\0';
+  
+  urlinfo->port = port;
+  urlinfo->path = path;
+  urlinfo->hostname = hostname;
 
   return urlinfo;
 }
@@ -71,8 +81,19 @@ int send_request(int fd, char *hostname, char *port, char *path)
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  int request_length = sprintf(request,"GET /%s HTTP/1.1\nHost: %s:%s\nConnection: close\n\n", 
+  path, 
+  hostname, 
+  port);
 
-  return 0;
+   rv = send(fd, request, request_length, 0);
+
+
+  if (rv < 0) {
+      perror("send");
+  }
+
+  return rv;
 }
 
 int main(int argc, char *argv[])
