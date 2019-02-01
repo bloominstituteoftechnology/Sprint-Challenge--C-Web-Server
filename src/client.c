11aa5c8b99764a6800ffe455af56023a24f5acb4
Timothy Hoang <timh1203@gmail.com>
2019-02-01 10:20:09 -0600

    Add parse_url()

--- src/client.c
+++ src/client.c
@@ -37,17 +37,27 @@ urlinfo_t *parse_url(char *url)
   /*
     We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).✅
+    2. Set the path pointer to 1 character after the spot returned by strchr.✅
+    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.✅
+    4. Use strchr to find the first colon in the URL.✅
+    5. Set the port pointer to 1 character after the spot returned by strchr.✅
+    6. Overwrite the colon with a '\0' so that we are just left with the hostname.✅
+    7. Set the information inside the struct✅
+    
+    EXAMPLE: localhost:3490/d20
   */
+  char *backslash = strchr(hostname, '/');
+  path = backslash + 1;
+  *backslash = '\0';
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  char *colon = strchr(hostname, ':');
+  port = colon + 1;
+  *colon = '\0';
+
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
