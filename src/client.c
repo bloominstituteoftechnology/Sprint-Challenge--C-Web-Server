be1a45f4cd3ccf5945e4f52331563c7d2389ef29
verydecent <wnjhwng@gmail.com>
2019-02-01 09:48:25 -0800

    send_req

--- src/client.c
+++ src/client.c
@@ -90,8 +90,18 @@ int send_request(int fd, char *hostname, char *port, char *path)
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  int request_length = sprintf(request,
+                                "GET /%s HTTP/1.1\n",
+                                "Host: %s:%s\n",
+                                "Connection: close",
+                                path, hostname, port);
 
-  return 0;
+  rv = send(fd, request, request_length, 0);
+
+  if (rv < 0) {
+      perror("send");
+  }
+  return rv;
 }
 
 int main(int argc, char *argv[])
