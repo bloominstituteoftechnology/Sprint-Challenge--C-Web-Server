af9ebf507af34354b5438dba891ed5870d598ff4
Tsai Huang <tsaihuangsd@gmail.com>
2019-02-08 14:46:36 -0800

    Upload fixes for main function

--- src/client.c
+++ src/client.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "lib.h"
+ #include <arpa/inet.h>
 
 
 #define BUFSIZE 4096 // max number of bytes we can get at once
@@ -80,10 +81,10 @@ int send_request(int fd, char *hostname, char *port, char *path)
   int request_length;
  
   // Build HTTP request and store it in request
-  char *full_path;
+  char full_path[50];
   strcpy(full_path,"/");
   strcat(full_path, path);
-  char *full_hostname;
+  char full_hostname[50];
   strcpy(full_hostname, hostname);
   strcat(full_hostname, ":");
   strcat(full_hostname, port);
@@ -91,7 +92,7 @@ int send_request(int fd, char *hostname, char *port, char *path)
   sprintf(request, "GET %s HTTP/1.1\nHost: %s\nConnection: close\n", full_path, full_hostname);
 
   request_length = strlen(request);
-  // printf("Request: %s, request length: %d\n", request, request_length);
+  // printf("Request: \n%s, request length: %d\n", request, request_length);
   int rv = send(fd, request, request_length+1, 0);
   if (rv < 0) {
       perror("send");
@@ -100,6 +101,15 @@ int send_request(int fd, char *hostname, char *port, char *path)
   return rv;
   // return 0;
 }
+
+// void *get_in_addr(struct sockaddr *sa)
+// {
+//     if (sa->sa_family == AF_INET) {
+//         return &(((struct sockaddr_in*)sa)->sin_addr);
+//     }
+
+//     return &(((struct sockaddr_in6*)sa)->sin6_addr);
+// }
 
 int main(int argc, char *argv[])
 {  
@@ -122,19 +132,38 @@ int main(int argc, char *argv[])
     }
     printf("webserver: waiting for connections on port %s...\n", url_parsed->port);
     
-    printf("send socket: %d, hostname: %s, path: %s, port: %s\n", sockfd, url_parsed->hostname, url_parsed->path, url_parsed->port);
+    // printf("send socket: %d, hostname: %s, path: %s, port: %s\n", sockfd, url_parsed->hostname, url_parsed->path, url_parsed->port);
     // 3. Call `send_request` to construct the request and send it
     send_request(sockfd, url_parsed->hostname, url_parsed->port, url_parsed->path);
     
     // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
-    // int recvfd;
+    // int recvfd = get_socket(url_parsed->hostname, url_parsed->port);
+    int buffer_length = strlen(buf);
     // struct sockaddr_storage their_addr; // connector's address information
-    // char s[INET6_ADDRSTRLEN];
+    while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0) {
+      fwrite(buf, 1, numbytes, stdout);
+    }
+    fclose(stdout);
     // while(1){
+    //   printf("here");
       // socklen_t sin_size = sizeof their_addr;
-      // recvfd = recv(sockfd, )
+      // recvfd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
+      // if (recvfd == -1) {
+      //     perror("accept");
+      //     exit(1);
+      // }
+      // int rv = recv(recvfd, buffer, buffer_length, 0);
+      // if (rv < 0) {
+      //   perror("send");
+      // }
+      // inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),s, sizeof s);
+      // printf("client: got connection from %s\n", s);
+      // printf("buffer: %s\n", buffer);
+      // fprintf(stdout, "%s\n", buffer);
+      // 5. Clean up any allocated memory and open file descriptors.
+    //   close(sockfd);
+      
     // }
-    // 5. Clean up any allocated memory and open file descriptors.
 
   return 0;
 }
