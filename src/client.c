d927ffab7846707877499a1903e0821936ce17c7
Hunter <huntersmith_15@hotmail.com>
2019-03-08 09:35:29 -0700

    Parse the input URL

--- src/client.c
+++ src/client.c
@@ -34,20 +34,27 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+ 
+    //We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    //1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+    char *back = strchr(hostname, '/');
+    //2. Set the path pointer to 1 character after the spot returned by strchr.
+    path = back + 1;
+    //3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+    *back = '\0';
+    //4. Use strchr to find the first colon in the URL.
+    char *colon = strchr(hostname, ':');
+    //5. Set the port pointer to 1 character after the spot returned by strchr.
+    port = colon + 1;
+    //6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+    *colon = '\0';
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+    //store the strings inside urlinfo to access for my socket
+    urlinfo->hostname = hostname;
+    urlinfo->path = path;
+    urlinfo->port = port;
+
 
   return urlinfo;
 }
