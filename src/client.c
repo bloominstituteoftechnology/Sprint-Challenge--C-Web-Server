3a7b08935a213773f58bc52de4bb504abf982339
Max <maxkajiwara@gmail.com>
2019-03-08 10:39:07 -0800

    parse url

--- src/client.c
+++ src/client.c
@@ -12,7 +12,8 @@
 /**
  * Struct to hold all three pieces of a URL
  */
-typedef struct urlinfo_t {
+typedef struct urlinfo_t
+{
   char *hostname;
   char *port;
   char *path;
@@ -34,20 +35,26 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+  // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+  char *backslash = strchr(hostname, '/');
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+  // 2. Set the path pointer to 1 character after the spot returned by strchr.
+  path = backslash + 1;
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+  *backslash = '\0';
+
+  // 4. Use strchr to find the first colon in the URL.
+  char *colon = strchr(hostname, ':');
+
+  // 5. Set the port pointer to 1 character after the spot returned by strchr.
+  port = colon + 1;
+
+  // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  *colon = '\0';
+
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
@@ -76,12 +83,13 @@ int send_request(int fd, char *hostname, char *port, char *path)
 }
 
 int main(int argc, char *argv[])
-{  
-  int sockfd, numbytes;  
+{
+  int sockfd, numbytes;
   char buf[BUFSIZE];
 
-  if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
 
