b9052fd97e11acbce0755471def33f4167893c16
Drew <drewsup123c@gmail.com>
2019-03-08 09:49:26 -0700

    finished parse url, started work on send_request

--- src/client.c
+++ src/client.c
@@ -33,21 +33,24 @@ urlinfo_t *parse_url(char *url)
   char *path;
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
+    // We can parse the input URL by doing the following:
+    // 1. Use strchr to find the first backslash in the URL 
+    // (this is assuming there is no http:// or https:// in the URL).
+  char *backslash = strchr(hostname, '/');
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
+  path = backslash + 1;
+    // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+  *backslash = '\0';
+    // 4. Use strchr to find the first colon in the URL.
+  char *colon = strchr(hostname, '/');
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+  port = colon + 1;
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  *colon = '\0';
 
-  /*
-    We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
@@ -67,11 +70,8 @@ int send_request(int fd, char *hostname, char *port, char *path)
   const int max_request_size = 16384;
   char request[max_request_size];
   int rv;
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
-
+  int request_length = sprintf(request, "GET %s HTTP/1.1\nHost: %s:%s\nConnection: Close\n\n",path,hostname,port);
+  get_socket();//hostname, port
   return 0;
 }
 
