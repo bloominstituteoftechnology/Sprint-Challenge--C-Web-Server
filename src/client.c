50fd52f45d959c46077a9182384b32dbd9c71d44
lndubose <lndubose13@gmail.com>
2019-03-08 11:23:29 -0600

    Receive the response from the server and print it to stdout

--- src/client.c
+++ src/client.c
@@ -123,5 +123,10 @@ int main(int argc, char *argv[])
   urlinfo_t *urlinfo = parse_url(argv[1]);
   sockfd = get_socket(urlinfo->hostname, urlinfo->port);
   send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+
+  while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0)
+  {
+    fprintf(stdout, "%s", buf);
+  }
   return 0;
 }
