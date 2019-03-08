ef559471f8e3f8a468460fbe444410ec3b6812de
ckirkwood94 <ckirkwood94@gmail.com>
2019-03-08 12:53:30 -0500

    Implement main()

--- src/client.c
+++ src/client.c
@@ -39,18 +39,18 @@ urlinfo_t *parse_url(char *url)
 
   // Find first backslash in URL
   // TODO: Check for protocol in URL (ie. http://, https://)
-  slash = strchr(hostname, "/");
+  slash = strchr(hostname, '/');
   // set path pointer to 1 character after slash
   path = slash + 1;
   // Set backslash to '\0'
   *slash = '\0';
 
   // Find first colon in URL
-  colon = strchr(hostname, ":");
+  colon = strchr(hostname, ':');
   // set port to 1 character after the colon
   port = colon + 1;
   // Set colon to '\0'
-  colon = '\0';
+  *colon = '\0';
 
   // Assign to fields on urlinfo
   urlinfo->hostname = hostname;
@@ -86,7 +86,7 @@ int send_request(int fd, char *hostname, char *port, char *path)
                                port);
 
   // Send it all!
-  int rv = send(fd, request, request_length, 0);
+  rv = send(fd, request, request_length, 0);
 
   if (rv < 0)
   {
@@ -115,9 +115,25 @@ int main(int argc, char *argv[])
     5. Clean up any allocated memory and open file descriptors.
   */
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // Parse input URL
+  urlinfo_t *urlinfo = parse_url(argv[1]);
+  // Initialize socket
+  sockfd = get_socket(urlinfo->hostname, urlinfo->port);
+  // call send_request to contruct the request and send it
+  send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+  // Call 'recv' in a loop until there is no more data to receive from the server
+  while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0)
+  {
+    // Print out response to stdout
+    fprintf(stdout, "%s\n", buf);
+  }
+
+  // Clean allocated memory
+  free(urlinfo->hostname);
+  free(urlinfo);
+
+  // Close file descriptor
+  close(sockfd);
 
   return 0;
 }
