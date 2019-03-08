561919aae7352d332c7e4104e0dab8f2853fba79
ckirkwood94 <ckirkwood94@gmail.com>
2019-03-08 12:19:58 -0500

    Implemented parse_url

--- src/client.c
+++ src/client.c
@@ -12,7 +12,8 @@
 /**
  * Struct to hold all three pieces of a URL
  */
-typedef struct urlinfo_t {
+typedef struct urlinfo_t
+{
   char *hostname;
   char *port;
   char *path;
@@ -31,23 +32,30 @@ urlinfo_t *parse_url(char *url)
   char *hostname = strdup(url);
   char *port;
   char *path;
+  char *slash;
+  char *colon;
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+  // Find first backslash in URL
+  // TODO: Check for protocol in URL (ie. http://, https://)
+  slash = strchr(hostname, "/");
+  // set path pointer to 1 character after slash
+  path = slash + 1;
+  // Set backslash to '\0'
+  *slash = '\0';
 
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+  // Find first colon in URL
+  colon = strchr(hostname, ":");
+  // set port to 1 character after the colon
+  port = colon + 1;
+  // Set colon to '\0'
+  colon = '\0';
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // Assign to fields on urlinfo
+  urlinfo->hostname = hostname;
+  urlinfo->path = path;
+  urlinfo->port = port;
 
   return urlinfo;
 }
@@ -76,12 +84,13 @@ int send_request(int fd, char *hostname, char *port, char *path)
 }
 
 int main(int argc, char *argv[])
-{  
-  int sockfd, numbytes;  
+{
+  int sockfd, numbytes;
   char buf[BUFSIZE];
 
-  if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
 
