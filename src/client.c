726f57caaea7aba0824ba9c2bdf328ae8d28e5c1
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-03-15 10:29:59 -0500

    Parse the input URL

--- src/client.c
+++ src/client.c
@@ -12,7 +12,8 @@
 /**
  * Struct to hold all three pieces of a URL
  */
-typedef struct urlinfo_t {
+typedef struct urlinfo_t
+{
   char *hostname;
   char *port;
   char *path;
@@ -32,11 +33,12 @@ urlinfo_t *parse_url(char *url)
   char *port;
   char *path;
 
+  printf("URL: %s\n", hostname);
+
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
   /*
     We can parse the input URL by doing the following:
-
     1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
     2. Set the path pointer to 1 character after the spot returned by strchr.
     3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
@@ -44,10 +46,22 @@ urlinfo_t *parse_url(char *url)
     5. Set the port pointer to 1 character after the spot returned by strchr.
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
+  path = strrchr(hostname, '/');
+  *path = '\0';
+  path++;
+  port = strrchr(hostname, ':');
+  *port = '\0';
+  port++;
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  if (hostname[0] == 'h')
+  {
+    hostname = strrchr(hostname, '/');
+    hostname++;
+  }
+
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
@@ -76,12 +90,13 @@ int send_request(int fd, char *hostname, char *port, char *path)
 }
 
 int main(int argc, char *argv[])
-{  
-  int sockfd, numbytes;  
+{
+  int sockfd, numbytes;
   char buf[BUFSIZE];
 
-  if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
 
@@ -93,9 +108,7 @@ int main(int argc, char *argv[])
     5. Clean up any allocated memory and open file descriptors.
   */
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  parse_url(argv[1]);
 
   return 0;
 }
